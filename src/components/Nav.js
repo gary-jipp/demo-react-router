fc16d6231adc0dba7f3f5f4cd10582356ac1ccdc
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2021-02-12 17:34:23 -0800

    Update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,10 +4,11 @@ import { Link, NavLink } from 'react-router-dom';
 const state1 = { page1State: true };
 const state2 = { page2State: true };
 function Nav() {
+  
   return (
     <nav className="nav-wrapper blue">
       <div className="container">
-        <a href="/#" className="brand-logo left">Material</a>
+        <a href="/#" className="brand-logo left">My App</a>
         <ul className="right">
           <li><Link to="/"> Home</Link></li>
           <li><NavLink to={{ pathname: "/page1", prop: 1, state1: state1 }}>Page1</NavLink></li>
